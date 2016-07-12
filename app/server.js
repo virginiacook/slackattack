334961027a92f3fb9e2e8ed74e8be39e8eb82776
virginiacook <virg.cook@earthlink.net>
2016-07-12 12:39:46 -0400

    more updates

--- app/server.js
+++ app/server.js
@@ -9,12 +9,12 @@ const yelp = new Yelp({
   token_secret: 'uuUPP1-W01p0igV0NngEBNMd8qU',
 });
 // See http://www.yelp.com/developers/documentation/v2/search_api
-yelp.search({ term: 'food', location: 'Montreal' })
-.then((data) => {
-  data.businesses.forEach(business => {
-    // do something with business
-  });
-});
+// yelp.search({ term: 'food', location: '03755' })
+// .then((data) => {
+//   data.businesses.forEach(business => {
+//     // do something with business
+//   });
+// });
 
 console.log('starting bot');
 // botkit controller
@@ -39,16 +39,30 @@ controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
   });
 });
 // example hello response
+controller.hears(['hungry'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  yelp.search({ term: 'food', location: '03755' })
+      .then((data) => {
+        bot.reply(message, 'Here are some options!');
+        data.businesses.forEach(business => {
+          // do something with business
+          // bot.reply(message, `Hello, ${business.name}!`);
+          bot.reply(message, {
+            'attachments': [
+              {
+                'title': business.name,
+                'image_url': business.image_url,
+                'text': business.snippet_text,
+                'color': '#7CD197',
+              },
+            ],
+            //'icon_url': 'http://lorempixel.com/48/48',
+          });
+        });
+      });
+});
 controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
   bot.api.users.info({ user: message.user }, (err, res) => {
     if (res) {
-      yelp.search({ term: 'food', location: 'Montreal' })
-      .then((data) => {
-        data.businesses.forEach(business => {
-          // do something with business
-          bot.reply(message, `Hello, ${business.name}!`);
-        });
-      });
       bot.reply(message, `Hello, ${res.user.name}!`);
     } else {
       bot.reply(message, 'Hello there!');
@@ -56,12 +70,7 @@ controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 
   });
 });
 
-controller.hears(['keyword', '^pattern$'], ['message_received'], function (bot, message) {
-  // do something to respond to message
-  // ...
 
-  bot.reply(message, 'Tell me more!');
-});
 controller.hears('open the (.*) doors', ['message_received'], (bot, message) => {
   const doorType = message.match[1]; // match[1] is the (.*) group. match[0] is the entire group (open the (.*) doors).
   if (doorType === 'pod bay') {
@@ -79,6 +88,9 @@ controller.on('direct_message', (bot, message) => {
   // reply to _message_ by using the _bot_ object
   bot.reply(message, 'You are talking directly to me');
 });
-controller.on('message_received', (bot, message) => {
-  bot.reply(message, 'I heard... something!');
+// controller.on('message_received', (bot, message) => {
+//   bot.reply(message, 'I heard... something!');
+// });
+controller.on('outgoing_webhook', (bot, message) => {
+  bot.replyPublic(message, 'yeah yeah');
 });
