dc9baeb0119adae6ee7e5fe1bd99e15914455a96
virginiacook <virg.cook@earthlink.net>
2016-07-12 14:34:04 -0400

    changes

--- app/server.js
+++ app/server.js
@@ -1,22 +1,21 @@
-import botkit from 'botkit';
-// Request API access: http://www.yelp.com/developers/getting_started/api_access
-const Yelp = require('yelp');
+/* some basic code and inspiration taken from https://github.com/dartmouth-cs52/slackattack
+and https://github.com/howdyai/botkit/blob/master/readme-slack.md#slack-specific-events
+thank you! */
 
+import botkit from 'botkit';
+
+// yelp setup
+const Yelp = require('yelp');
 const yelp = new Yelp({
   consumer_key: '-_1KpAEOaNwru8fE66TbcQ',
   consumer_secret: 'bFkWcgL0_03XeMdBPmd4_suW5Ck',
   token: 'wEHDFLc4Mw9OUYgG9CoA1NH2Q4hLyjN_',
   token_secret: 'uuUPP1-W01p0igV0NngEBNMd8qU',
 });
-// See http://www.yelp.com/developers/documentation/v2/search_api
-// yelp.search({ term: 'food', location: '03755' })
-// .then((data) => {
-//   data.businesses.forEach(business => {
-//     // do something with business
-//   });
-// });
 
+// write to console that bot is starting
 console.log('starting bot');
+
 // botkit controller
 const controller = botkit.slackbot({
   debug: false,
@@ -38,14 +37,13 @@ controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
     if (err) { throw new Error(err); }
   });
 });
-// example hello response
+
+// response for food
 controller.hears(['hungry'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
   yelp.search({ term: 'food', location: '03755' })
       .then((data) => {
         bot.reply(message, 'Here are some options!');
         data.businesses.forEach(business => {
-          // do something with business
-          // bot.reply(message, `Hello, ${business.name}!`);
           bot.reply(message, {
             'attachments': [
               {
@@ -55,11 +53,12 @@ controller.hears(['hungry'], ['direct_message', 'direct_mention', 'mention'], (b
                 'color': '#7CD197',
               },
             ],
-            //'icon_url': 'http://lorempixel.com/48/48',
           });
         });
       });
 });
+
+// response to hello
 controller.hears(['hello', 'hi', 'hey', 'howdy'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
   bot.api.users.info({ user: message.user }, (err, res) => {
     if (res) {
@@ -74,19 +73,20 @@ controller.hears(['help'], ['direct_message'], (bot, message) => {
     bot.reply(message, 'Tell me you\'re hungry and I\'ll tell you where to eat!');
   });
 });
+
 // reply to a direct mention - @bot hello
 controller.on('direct_mention', (bot, message) => {
   // reply to _message_ by using the _bot_ object
   bot.reply(message, 'Thanks for mentioning me!');
 });
+
 // reply to a direct message
 controller.on('direct_message', (bot, message) => {
   // reply to _message_ by using the _bot_ object
   bot.reply(message, 'What are you talking about? Tell me you\'re hungry and I\'ll tell you where to eat!');
 });
-// controller.on('message_received', (bot, message) => {
-//   bot.reply(message, 'What are you talking about? Tell me you\'re hungry and I\'ll tell you where to eat!');
-// });
+
+// response to outgoing webhook
 controller.on('outgoing_webhook', (bot, message) => {
   bot.replyPublic(message, 'Good morning!');
   bot.reply(message, 'Good morning!');
